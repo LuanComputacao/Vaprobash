1e5242794de4672b1e2912e68f73fbeb14df069a
Luan Roger Santos Santana <luanengcomputacao@gmail.com>
2015-08-31 13:37:30 -0300

    php5-fpm

--- scripts/php.sh
+++ scripts/php.sh
@@ -130,8 +130,8 @@ else
         echo ">>>>>> Copy of php.ini-development to /usr/local/lib/php.ini"
         sudo cp /opt/build/php-${PHP_VERSION}.29/php.ini-development /usr/local/lib/php.ini
 
-#        echo ">>>>>> Installing PHP-FPM"
-#        sudo apt-get -y install php5-fpm
+        echo ">>>>>> Installing PHP-FPM"
+        sudo apt-get -y install php5-fpm
 
 
     else
@@ -157,19 +157,19 @@ else
 
     # xdebug Config
     cat > $(find /etc/php5 -name xdebug.ini) << EOF
-zend_extension=$(find /usr/lib/php5 -name xdebug.so)
-xdebug.remote_enable = 1
-xdebug.remote_connect_back = 1
-xdebug.remote_port = 9000
-xdebug.scream=0
-xdebug.cli_color=1
-xdebug.show_local_vars=1
+                                                zend_extension=$(find /usr/lib/php5 -name xdebug.so)
+                                                xdebug.remote_enable = 1
+                                                xdebug.remote_connect_back = 1
+                                                xdebug.remote_port = 9000
+                                                xdebug.scream=0
+                                                xdebug.cli_color=1
+                                                xdebug.show_local_vars=1
 
-; var_dump display
-xdebug.var_display_max_depth = 5
-xdebug.var_display_max_children = 256
-xdebug.var_display_max_data = 1024
-EOF
+                                                ; var_dump display
+                                                xdebug.var_display_max_depth = 5
+                                                xdebug.var_display_max_children = 256
+                                                xdebug.var_display_max_data = 1024
+                                                EOF
 
     # PHP Error Reporting Config
     sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
