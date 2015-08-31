88299ad67d6cfe119f63498c4de83c2900b22bd5
Luan Roger Santos Santana <luanengcomputacao@gmail.com>
2015-08-31 13:40:39 -0300

    revert tab

--- scripts/php.sh
+++ scripts/php.sh
@@ -117,7 +117,7 @@ else
         # Install PHP
         echo ">>>>>> Making PHP $PHP_VERSION"
         sudo make
-        #sudo make test
+        sudo make test
 
         #sudo make test
         echo ">>>>>> Installing PHP $PHP_VERSION"
@@ -157,19 +157,19 @@ else
 
     # xdebug Config
     cat > $(find /etc/php5 -name xdebug.ini) << EOF
-                                                zend_extension=$(find /usr/lib/php5 -name xdebug.so)
-                                                xdebug.remote_enable = 1
-                                                xdebug.remote_connect_back = 1
-                                                xdebug.remote_port = 9000
-                                                xdebug.scream=0
-                                                xdebug.cli_color=1
-                                                xdebug.show_local_vars=1
+zend_extension=$(find /usr/lib/php5 -name xdebug.so)
+xdebug.remote_enable = 1
+xdebug.remote_connect_back = 1
+xdebug.remote_port = 9000
+xdebug.scream=0
+xdebug.cli_color=1
+xdebug.show_local_vars=1
 
-                                                ; var_dump display
-                                                xdebug.var_display_max_depth = 5
-                                                xdebug.var_display_max_children = 256
-                                                xdebug.var_display_max_data = 1024
-                                                EOF
+; var_dump display
+xdebug.var_display_max_depth = 5
+xdebug.var_display_max_children = 256
+xdebug.var_display_max_data = 1024
+EOF
 
     # PHP Error Reporting Config
     sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
